f6ed54c9110b71d5b502268b89c16fe023c009fc
Nyhazzy <kimnayeon0525@gmail.com>
2025-04-16 18:50:36 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -4,17 +4,22 @@
 # 그러니까 카드 뭉치 내의 2번째 단어를 사용하려면 첫번째거를 무조건 사용해야한다. -> 앞에거 부터 써야된다 ; Queue
 
 from collections import deque
+
 def solution(cards1, cards2, goal):
-    
-    cards1, cards2 = deque(cards1), deque(cards2)
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
     
     for word in goal:
         if cards1 and cards1[0] == word:
             cards1.popleft()
-            
         elif cards2 and cards2[0] == word:
             cards2.popleft()
         else:
             return "No"
-        
+    
     return "Yes"
+            
+    
+    
+    
+   
